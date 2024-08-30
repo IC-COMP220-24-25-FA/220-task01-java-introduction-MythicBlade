d1c53c2aaac543aa53ce975bff09dd6be1eace58
MythicBlade <85843085+MythicBlade@users.noreply.github.com>
2024-08-30 00:32:28 -0400

    created the Test file for the Rectangle class. Created a test for the Rectangle constructer.

---
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -0,0 +1,15 @@
+package edu.ithaca.dragon.shapes;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+public class RectangleTest {
+    
+    @Test
+    public void TestConstructorError(){
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(0,1));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(1,0));
+    }
+}
