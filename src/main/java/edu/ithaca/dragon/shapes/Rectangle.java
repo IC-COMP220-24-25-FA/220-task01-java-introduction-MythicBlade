e2ef5f781e0973f2a2d3824346c1ccbf6be92fa1
MythicBlade <85843085+MythicBlade@users.noreply.github.com>
2024-08-30 00:46:12 -0400

    fixed an issue with Rectangle constructer

--- src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
@@ -1,12 +1,15 @@
 package edu.ithaca.dragon.shapes;
 
 public class Rectangle {
-    
+    private double length;
+    private double width;
 
     public Rectangle(double length, double width){
         if (length <= 0 || width <= 0) {
             throw new IllegalArgumentException("invalid dimension");
         }
+        this.length = length;
+        this.width = width;
     }
 
     public double calcArea(){
