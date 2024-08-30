9ce8b2ef2b682971b85cc68d361b9d561059fb07
MythicBlade <85843085+MythicBlade@users.noreply.github.com>
2024-08-30 00:20:43 -0400

    Implemented the checks for radius in the constructor of circle

--- src/main/java/edu/ithaca/dragon/shapes/Circle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Circle.java
@@ -7,6 +7,10 @@ public class Circle {
      * @throws IllegalArgumentException if radius is not a positive number
      */
     public Circle(double radius){
+        if (radius <= 0){
+            throw new IllegalArgumentException("invalid radius");
+        }
+        
         this.radius = radius;
     }
 
