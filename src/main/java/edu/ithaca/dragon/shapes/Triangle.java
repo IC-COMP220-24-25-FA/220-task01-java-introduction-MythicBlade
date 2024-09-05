ef7a9510464c0e0bd5fbf84d95aa28d626b3521c
MythicBlade <85843085+MythicBlade@users.noreply.github.com>
2024-09-05 10:58:05 -0400

    I added the Triangle and TriangleTest classes. I also made a few fixes and added the first test to TriangleTest.

---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,10 @@
+package edu.ithaca.dragon.shapes;
+
+public class Triangle {
+    private double sideLength;
+
+    public Triangle(double sideLength_){
+        sideLength = sideLength_;
+    }
+    
+}
---
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -0,0 +1,18 @@
+package edu.ithaca.dragon.shapes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TriangleTest {
+    public TriangleTest() {
+    }
+
+    @Test
+    public void calcAreaTest(){
+        Triangle myTriangle =  new Triangle(1);
+        assertEquals(0.866025, myTriangle.calcArea(),0.01);
+    }
+
+}
