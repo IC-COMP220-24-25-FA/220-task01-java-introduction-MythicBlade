8d6d1d1ff7760258f11926efd3b1f85f86c6bc02
MythicBlade <85843085+MythicBlade@users.noreply.github.com>
2024-09-06 11:35:57 -0400

    Implimented the calcArea method

--- src/main/java/edu/ithaca/dragon/shapes/Triangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -1,10 +1,16 @@
 package edu.ithaca.dragon.shapes;
-
 public class Triangle {
     private double sideLength;
 
+    /*
+ * This Triangle is only an equalateral triangle with the stated side length. 
+ * It could be adjusted to a trangle with more complex sides however this seemed like the simplest version of this.
+ */
     public Triangle(double sideLength_){
         sideLength = sideLength_;
     }
-    
+    public double calcArea(){
+        return (Math.sqrt(3)*sideLength)/2;
+    }
+
 }
